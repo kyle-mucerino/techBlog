63f14dd25b4ceaf6c1d169afab2e7280e315f03d
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-06 14:43:37 -0700

    README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # techBlog
+
+I turned this repo in but was not satisfied with the work I had done on it so I wanted to restart the assigment but had to turn this in so it would not appear late in my bootcamp spot. I'll attach the new github repo and deployed app below for this assignment I ended up using. Thank you for your hard work and I'm sorry for the inconvenience this may have caused.
+
+new github repo: https://github.com/kyle-mucerino/tech-blog.git
+
+url: http://localhost:3001/
+
--- server.js
+++ server.js
@@ -1,44 +1,51 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+// const path = require('path');
+// const express = require('express');
+// const session = require('express-session');
+// const exphbs = require('express-handlebars');
+// const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/config');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const sequelize = require('./config/config');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
-const PORT = process.envPORT || 3001;
+// const app = express();
+// const PORT = process.envPORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {
-    maxAge: 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict'
-  },
-  resave: false,
-  saveUnitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {
+//     maxAge: 24 * 60 * 60 * 1000,
+//     httpOnly: true,
+//     secure: false,
+//     sameSite: 'strict'
+//   },
+//   resave: false,
+//   saveUnitialized: false,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
 
-app.use(session(sess));
+// app.use(
+//   session({
+//     secret: "Super secret secret",
+//     resave: false,
+//     saveUninitialized: false
+//   })
+// );
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handelbars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handelbars');
 
-app.use(require('./controllers/'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// app.use(require('./controllers/'));
+
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
